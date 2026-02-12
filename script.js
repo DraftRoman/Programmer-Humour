8ec120d686b09fb09c010d8d033684410af24726
Roman <roman.boxes@gmail.com>
2026-02-12 14:44:15 +0100

    Add title and subtitle to this function
    Trying to get as maximum as possible from  this small object

--- script.js
+++ script.js
@@ -3,11 +3,15 @@ import { getData } from "./app.js";
 
 async function setup() {
     const theObject = await getData();
+    const root = document.getElementById("main");
+    const title = document.createElement("h1");
+    const suptitle = document.createElement("h3");
+    title.textContent = theObject.title
+    suptitle.textContent = " day " + theObject.day;
     const myImage = new Image(400, 400);
     myImage.src = theObject.img;
     myImage.alt = theObject.alt;
-    const root = document.getElementById("main");
-    root.append(myImage);
+    root.append(myImage, title, suptitle);
     console.log(theObject);
 }
 setup()
