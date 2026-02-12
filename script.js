21196b1c4557b6d4dc7b17efd4a67027b59114fb
Roman <roman.boxes@gmail.com>
2026-02-12 14:28:15 +0100

    setup function
    - create a frame for a picture
    - catch the link and alternative text for image
    - display it on the main on page
    Create a file style.css

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ import { getData } from "./app.js";
 
 async function setup() {
     const theObject = await getData();
+    const myImage = new Image(400, 400);
+    myImage.src = theObject.img;
+    myImage.alt = theObject.alt;
+    const root = document.getElementById("main");
+    root.append(myImage);
     console.log(theObject);
 }
 setup()
